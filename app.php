19c72610f3b08b0459e49c249f0a450b6e58c72a
Jose Carneiro <altibajo.87@gmail.com>
2014-01-19 20:52:02 +0000

    correcion en la clase suma

---
+++ app.php
@@ -0,0 +1,12 @@
+<?php
+namespace Hackspace;
+include 'suma.php';
+
+use Hackspace\Suma;
+
+# operacion1 sumar 3,4
+# operacion2 suma  5,8
+
+$suma = new Suma(3,4);
+echo $suma->ejecutar();
+echo "\n";
